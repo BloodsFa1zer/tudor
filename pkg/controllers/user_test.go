a8658227a236a3717d04d74d8a93790b50854f96
Bogdan Pavliuk <bopvlk@gmail.com>
2024-01-08 14:18:38 +0000

    Refactor user_test.go controller functions

--- pkg/controllers/user_test.go
+++ pkg/controllers/user_test.go
@@ -18,7 +18,7 @@ var (
 	router = gin.New()
 
 	// this is a function that returns a UserController with a mocked UsersRepository
-	newTestUserController = func(db *mocks.MockUsersRepository) UserController {
+	newTestUserCtrller = func(db *mocks.MockUsersRepository) UserController {
 		return NewUsersController(services.NewUserService(
 			func(userid int64, userName string) (string, error) { return "token", nil },
 			func(password string) string { return password },
@@ -91,12 +91,72 @@ func TestUserRegister(t *testing.T) {
 	}
 	for _, tc := range testTable {
 		t.Run(tc.scenario, func(t *testing.T) {
-			usersRepo := newMockUsersRepository(t)
-			usersController := newTestUserController(usersRepo)
+			repo := newMockUsersRepository(t)
+			ctrller := newTestUserCtrller(repo)
 			gctx, w := newTestContext("POST", "/api/auth/register", tc.request)
 
-			usersRepo.EXPECT().CreateUser(gomock.Any(), tc.inputDBUser).Return(tc.expectedDBUser, tc.expectedError).AnyTimes()
-			usersController.UserRegister(gctx)
+			repo.EXPECT().CreateUser(gomock.Any(), tc.inputDBUser).Return(tc.expectedDBUser, tc.expectedError).AnyTimes()
+			ctrller.UserRegister(gctx)
+
+			if w.Code != tc.expectedStatusCode {
+				t.Errorf("expected status code %d, got %d", tc.expectedStatusCode, w.Code)
+			}
+			if strings.TrimSpace(w.Body.String()) != tc.expectedResponse {
+				t.Errorf("expected response %s, got %s", tc.expectedResponse, w.Body.String())
+			}
+
+		})
+	}
+}
+
+func TestUserLogin(t *testing.T) {
+	now := time.Now()
+	testTable := []struct {
+		scenario           string
+		request            string
+		inputDBEmail       string
+		expectedDBUser     *entities.User
+		expectedResponse   string
+		expectedStatusCode int
+		expectedError      error
+	}{
+		{"success",
+			`{"email":"john@example.com","password": "123456"}`,
+			"john@example.com",
+			&entities.User{1, "John Doe", "john@example.com", "", true, "123456", "user", now, now},
+			`{"data":{"token":"token"},"status":"success"}`,
+			200,
+			nil},
+		{"failed_invalid_request",
+			`{"email":"`,
+			"",
+			nil,
+			`{"data":"unexpected EOF","status":"failed"}`,
+			400,
+			nil},
+		{"failed_invalid_email",
+			`{"email":"","name": "John Doe","password": "123456"}`,
+			"",
+			nil,
+			`{"data":"email and password required","status":"failed"}`,
+			400,
+			nil},
+		{"failed_can_not_fetch_user",
+			`{"email": "john@example.com", "name": "John Doe", "password": "123456"}`,
+			"john@example.com",
+			nil,
+			`{"data":"can not fetch user","status":"failed"}`,
+			401,
+			errors.New("can not fetch user")},
+	}
+	for _, tc := range testTable {
+		t.Run(tc.scenario, func(t *testing.T) {
+			repo := newMockUsersRepository(t)
+			controller := newTestUserCtrller(repo)
+			ctx, w := newTestContext("POST", "/api/auth/login", tc.request)
+
+			repo.EXPECT().GetUserByEmail(gomock.Any(), tc.inputDBEmail).Return(tc.expectedDBUser, tc.expectedError).AnyTimes()
+			controller.UserLogin(ctx)
 
 			if w.Code != tc.expectedStatusCode {
 				t.Errorf("expected status code %d, got %d", tc.expectedStatusCode, w.Code)
